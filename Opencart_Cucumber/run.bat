7bc88988f2f2650191e49b8d89c8e367f3cc4d96
Karthik-Paka <karthikpaka1435@gmail.com>
2024-07-10 12:08:59 +0530

    first commit

---
+++ Opencart_Cucumber/run.bat
@@ -0,0 +1,2 @@
+cd D:\Java\WorkSpace\Opencart_Cucumber
+mvn test
---
+++ Opencart_Cucumber/src/test/java/hooks/Hooks.java
@@ -0,0 +1,56 @@
+package hooks;
+
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import factory.BaseClass;
+import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
+
+public class Hooks {
+
+	 WebDriver driver;
+	 Properties p;
+     
+	@Before     // setup() will execute before execution all the steps
+    public void setup() throws IOException
+    {
+    	driver=BaseClass.initilizeBrowser();
+    	    	
+    	p=BaseClass.getProperties();
+    	driver.get(p.getProperty("appURL"));
+    	driver.manage().window().maximize();
+    			
+	}
+		
+    
+    @After   //tearDown() will execute after execution all the steps 
+    public void tearDown() {
+        		
+       driver.quit();
+       
+    }
+    
+
+    @AfterStep   // will execute after every step in Scenario
+    public void addScreenshot(Scenario scenario) {
+        
+    	// this is for cucumber junit report
+        if(scenario.isFailed()) {
+        	
+        	TakesScreenshot ts=(TakesScreenshot) driver;
+        	byte[] screenshot=ts.getScreenshotAs(OutputType.BYTES);
+        	scenario.attach(screenshot, "image/png",scenario.getName());
+        	            
+        }
+      
+    }
+   
+}
